9c665c2a3c26ca7ccacef10857d0be1b5162f721
Austen Dicken <cvpcsm@gmail.com>
2011-05-08 19:07:21 +0400

    use modified shortened list

--- tpl/default/list.php
+++ tpl/default/list.php
@@ -1,31 +1,31 @@
 <?php
 /**
- * default list template
+ * small list template
  *
  * This template is used by the <blog list> syntax and can be chosen
  * using the 'tpl' attribute. It is used to display a single entry in
  * the list and is called multiple times (once for each shown entry)
  *
- * This example shows full entries and add a footer with info
- * on tags and comments.
+ * This example shows only entry abstracts with comment numbers
  */
 ?>
 <div class="blogtng_list">
-<?php
-    if ($entry->tpl_entry(true, 'syntax', false)) {
-?>
-<div class="blogtng_footer level1">
-    <a href="<?php $entry->tpl_link()?>" class="wikilink1 blogtng_permalink"><?php $entry->tpl_title()?></a>
+  <h1>
+    <a href="<?php $entry->tpl_link()?>" class="blogtng_permalink">
+      [<?php $entry->tpl_created('%F')?>]
+      <?php $entry->tpl_title()?>
+    </a>
+  </h1>
+  <p>
+    <?php $entry->tpl_abstract()?><br />
+  </p>
+  <div class="blogtng_footer level1">
+    <a href="<?php $entry->tpl_link()?>" class="wikilink1 blogtng_permalink">Read more...</a>
     &middot;
-    <?php $entry->tpl_created('%Y-%m-%d %H:%M')?>
+    <?php $entry->tpl_created('%F %T')?>
     &middot;
     <?php $entry->tpl_author()?>
     &middot;
-    <a href="<?php $entry->tpl_link('the__comments')?>" class="wikilink1 blogtng_commentlink"><?php $entry->tpl_commentcount('%d Comments', '%d Comment','%d Comments')?></a>
-    &middot;
-    Tags: <?php $entry->tpl_tags('')?>
-</div>
-<?php
-    }
-?>
+    <a href="<?php $entry->tpl_link('the__comments')?>" class="wikilink1 blogtng_commentlink"><?php $entry->tpl_commentcount('%d Comments','%d Comment', '%d Comments')?></a>
+  </div>
 </div>
